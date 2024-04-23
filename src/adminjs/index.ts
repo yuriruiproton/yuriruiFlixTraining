12831bb03a161b8a0117090c559b3e9a9b764594
unknown <yurirui656@example.com>
2024-04-23 16:41:39 +0200

    Configurando o AdminJS

---
+++ src/adminjs/index.ts
@@ -0,0 +1,35 @@
+import AdminJs from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+import AdminJSSequelize from "@adminjs/sequelize"
+import AdminJS from "adminjs";
+import { sequelize } from "../database";
+
+AdminJs.registerAdapter(AdminJSSequelize)
+
+export const adminJs = new AdminJS({
+    databases: [sequelize],
+    rootPath: "/admin",
+    branding:{
+        companyName:'YURIRUIFLIX',
+        logo:'./2.png',
+        theme:{
+            colors:{
+                primary100: '#ff0043',
+                primary80: '#ff1a57',
+                primary60: '#ff3369',
+                primary40: '#ff4d7c',
+                  primary20: '#ff668f',
+                grey100: '#151515',
+                grey80: '#333333',
+                grey60: '#4d4d4d',
+                grey40: '#666666',
+                grey20: '#dddddd',
+                filterBg: '#333333',
+                accent: '#151515',
+                hoverBg: '#151515',
+            }
+        }
+    }
+})
+
+export const adminjsRouter = AdminJSExpress.buildRouter(adminJs)
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import exp from "constants"
-import express from 'express'
+import express from 'express';
+import { adminJs, adminjsRouter } from "./adminjs"
 import { sequelize } from "./database"
 
+
 const app = express()
+
+app.use(express.static('public'))
+
+app.use(adminJs.options.rootPath, adminjsRouter)
 
 const PORT = process.env.PORT || 3000
 
