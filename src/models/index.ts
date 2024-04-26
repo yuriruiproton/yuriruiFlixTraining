e562e53cecb84087ff18d856fc8f76e1bf43e6a0
unknown <yurirui656@example.com>
2024-04-26 20:57:47 +0200

    Criando a tabela Episodeos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,17 @@
 import { Category } from './Category';
 import { Course } from './Courses';
+import { Episode } from './Episodes';
 
 Category.hasMany(Course)
 
 Course.belongsTo(Category)
 
+Course.hasMany(Episode)
+
+Episode.belongsTo(Course)
+
 export {
     Category,
-    Course
+    Course,
+    Episode
 }
