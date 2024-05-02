a956eca9c4f5a5595601cc2f88eea608f1d9e1ab
unknown <yurirui656@example.com>
2024-05-02 13:05:44 +0200

    Upload de arquivos nos AdminJS

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -2,7 +2,8 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
+import e from "express";
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
@@ -15,6 +16,7 @@ export const adminJsResources: ResourceWithOptions[] = [
     },
     {
         resource: Episode,
-        options: episodeResourceOptions
+        options: episodeResourceOptions,
+        features: episodeResourceFeatures
     }
 ]
